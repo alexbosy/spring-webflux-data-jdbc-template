ec5d52d9f5549787525ca94d8becf06c18ea0c63
Alexander Bosy <alex.bosy@gmail.com>
2022-11-17 18:19:06 +0200

    added unit tests stub

---
+++ src/test/groovy/dev/abo/someservice/HealthCheckControllerSpec.groovy
@@ -0,0 +1,11 @@
+package dev.abo.someservice
+
+import spock.lang.Specification
+
+class HealthCheckControllerSpec extends Specification {
+
+    def "dummy test"() {
+        expect:
+        1 == 1
+    }
+}
